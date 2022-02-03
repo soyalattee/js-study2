e1947da1eeb266192e4e526c32a22913ef8f750e
박소연 <soyalatte@neowiz.com>
2022-02-03 18:43:30 +0900

    1주차

--- week01/main.js
+++ week01/main.js
@@ -1,5 +1,7 @@
 import { question } from "readline-sync";
+import utf8 from "utf8";
+const name = question("이름을 입력하세요...", {
+  encoding: "utf8",
+});
 
-const name = question("이름을 입력하세요...");
-
-console.log("Hello " + name);
+console.log(`안녕하세요 ${name}`);
