f09d929b498381214c28e94afce816c3b3f56c01
soyalattee <uyyu1734@gmail.com>
2022-02-10 20:45:01 +0900

    26번

--- week03/main.js
+++ week03/main.js
@@ -1,6 +1,6 @@
 import { question } from "readline-sync";
 
-const yellowColor = { name: "elie", age: 20, gsn: "141414", money: 300000 };
+const users = { name: "elie", age: 20, gsn: "141414", money: 300000 };
 
 console.log(users.money);
 
@@ -12,11 +12,16 @@ users.age = 25;
 console.log(users + `입니다`); // 문자랑 + 객체(변수)
 console.log(` ${users} 입니다`);
 
- a e u i o 
- 
- if ( str.charAt(0) == 'a' || str.charAt(0) == 'e' ){
-
- }
- else {
-
- }
+//26번 a e u i o
+const str = question("단어를 입력하세요...");
+if (
+  str.charAt(0) == "a" ||
+  str.charAt(0) == "e" ||
+  str.charAt(0) == "u" ||
+  str.charAt(0) == "i" ||
+  str.charAt(0) == "o"
+) {
+  console.log(str.concat("way"));
+} else {
+  console.log(str.slice(1) + str.charAt(0) + "ay");
+}
