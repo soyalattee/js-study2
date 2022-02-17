640f7d8dc2d07f76da36d95e44279df903009f7b
soyalattee <uyyu1734@gmail.com>
2022-02-17 15:20:36 +0900

    week04폴더 생성

--- week03/main.js
+++ week03/main.js
@@ -11,11 +11,11 @@ users.name = "soyeon";
 users.age = 25;
 console.log(users + `입니다`); // 문자랑 + 객체(변수)
 console.log(` ${users} 입니다`);
-
+// const meme = 1;
 //26번 a e u i o
 const str = question("단어를 입력하세요...");
 if (
-  str.charAt(0) == "a" ||
+   str.charAt(0) == "a" ||
   str.charAt(0) == "e" ||
   str.charAt(0) == "u" ||
   str.charAt(0) == "i" ||
